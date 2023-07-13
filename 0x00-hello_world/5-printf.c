d7894be5ab8b29062d4c960eefd2e128314aa35e
Tobintino <tobidelly@gmail.com>
2023-07-13 20:11:48 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,7 @@
-#include <studio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+#include <stdio.h>
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }

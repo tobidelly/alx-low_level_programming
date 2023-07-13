15420d052f1a59a6101c9da4d4067a70f1ef0b2e
Tobintino <tobidelly@gmail.com>
2023-07-13 20:17:09 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a specific phrase with proper grammar
+ * and states that the outcome is a piece of art.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

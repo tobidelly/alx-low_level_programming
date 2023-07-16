e6f8f9774ee0c18347a190772442bffda7344a85
Tobintino <tobidelly@gmail.com>
2023-07-16 18:07:45 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * A program that prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar('0' + i);
+
+		if (i != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

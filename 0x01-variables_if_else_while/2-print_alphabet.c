9fca22f13af42154f40da704aa4fb15e0de4975d
Tobintino <tobidelly@gmail.com>
2023-07-16 16:21:50 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Prints characters, alphabets in lowercases
+ *
+ * Return: Always(0) Success
+ */
+
+int main(void)
+{
+char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+		putchar('\n');
+
+	return (0);
+}

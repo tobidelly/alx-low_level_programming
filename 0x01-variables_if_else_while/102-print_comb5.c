5f9452a66caf6277d9ba884e9eece425c142ed21
Tobintino <tobidelly@gmail.com>
2023-07-16 19:40:02 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,18 +16,21 @@ int main(void)
 
 	for (a = 0; a < 100; a++)
 	{
-		for (b = a; b < 100; b++)
+		for (b = 0; b < 100; b++)
 		{
-		putchar('0' + (a / 10));
-		putchar('0' + (a % 10));
-		putchar(' ');
-		putchar('0' + (b / 10));
-		putchar('0' + (b % 10));
-
-			if (a != 98 || b != 99)
+			if (a < b)
 			{
-			putchar(',');
+			putchar('0' + (a / 10));
+			putchar('0' + (a % 10));
 			putchar(' ');
+			putchar('0' + (b / 10));
+			putchar('0' + (b % 10));
+
+				if (a != 98 || b != 99)
+				{
+				putchar(',');
+				putchar(' ');
+				}
 			}
 		}
 	}

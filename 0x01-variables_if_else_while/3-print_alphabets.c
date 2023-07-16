6246e6331743cb40eba6b4b46353f05e16abe4d8
Tobintino <tobidelly@gmail.com>
2023-07-16 16:32:06 +0000

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-print_alphabets.c -o 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * A program that prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+	putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

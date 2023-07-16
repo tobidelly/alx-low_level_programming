e820f5b6f4c9abb0aab67a64be192729cd6fefdb
Tobintino <tobidelly@gmail.com>
2023-07-16 17:38:22 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * A program that prints all the numbers of base 16 in lowercase, followed
+ * by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i);
+	}
+
+	for (i = 0; i < 6; i++)
+	{
+	putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

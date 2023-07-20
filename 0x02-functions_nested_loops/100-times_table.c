754217a54e8284ded233997d2b39437814f35a0f
Tobintino <tobidelly@gmail.com>
2023-07-20 16:40:20 +0000

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
+ */
+
+void print_times_table(int n)
+{
+	if (n < 0 || n > 15)
+	return;
+
+	for (int i = 0; i <= n; i++)
+	{
+	for (int j = 0; j <= n; j++)
+		{
+		int k = i * j;
+		if (j != 0)
+			{
+			_putchar(',');
+			_putchar(' ');
+			if (k < 10)
+			_putchar(' ');
+			}
+			if (k >= 100)
+			_putchar(k / 100 + '0');
+			if (k >= 10)
+			_putchar((k / 10) % 10 + '0');
+			_putchar(k % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}

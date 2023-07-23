bf32d8a55f76260e005316c5020f63f5e8a8dc63
Tobintino <tobidelly@gmail.com>
2023-07-23 16:31:43 +0000

    Debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * debugging test helps us tests codes
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("%d is ", n);
+	if (n > 0)
+	{
+	printf("positive\n");
+	}
+	else if (n == 0)
+	{
+	printf("zero\n");
+	}
+	else
+	{
+	printf("negative\n");
+	}
+
+	return 0;
+}

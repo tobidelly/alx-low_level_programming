ca4d404b2f6f7eb657804c414ec333de936a0e69
Tobintino <tobidelly@gmail.com>
2023-07-28 19:35:09 +0000

    More Pointers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -64,16 +64,17 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		overflow = 1;
 			else
 			overflow = 0;
-		if (digits >= (size_r - 1)
+		if (digits >= (size_r - 1))
 		return (0);
 			*(r + digits) = (temp_tot % 10) + '0';
 
 		digits++;
-		j - +;
+		j--;
 		i--;
 	}
-	if (digits == size_r
+	if (digits == size_r)
 		return (0);
-	*(r + digits) = '\0'ev_string(r);
+	*(r + digits) = '\0';
+	rev_string(r);
 	return (r);
 }

b3152c6d76a9ef1fb245a5d76aebb6d6a9db66e4
Tobintino <tobidelly@gmail.com>
2023-08-07 17:53:21 +0000

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -26,34 +26,30 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
+		while (cents >= 25)
+		{
+			cents -= 25;
+			coins++;
+		}
 
-	while (cents >= 25)
-	{
-		cents -= 25;
-		coins++;
-	}
+		while (cents >= 10)
+		{
+			cents -= 10;
+			coins++;
+		}
 
-	while (cents >= 10)
-	{
-		cents -= 10;
-		coins++;
-	}
+		while (cents >= 5)
+		{
+			cents -= 5;
+			coins++;
+		}
 
-	while (cents >= 5)
-	{
-		cents -= 5;
-		coins++;
-	}
-
-	while (cents >= 2)
-	{
-		cents -= 2;
-		coins++;
-	}
-
+		while (cents >= 2)
+		{
+			cents -= 2;
+			coins++;
+		}
 	coins += cents;
-
 	printf("%d\n", coins);
-
 	return (0);
 }

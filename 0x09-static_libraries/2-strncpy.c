85d5749f5872895b21cda2d07d3945d21973bd00
Tobintino <tobidelly@gmail.com>
2023-08-07 09:55:44 +0000

    Static Libraries

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string
+ * @dest: input value type
+ * @src: input value type
+ * @n: input value type
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}

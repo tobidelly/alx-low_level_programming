123edc28e75281f586f246a0c362fb4a2220b0bd
Tobintino <tobidelly@gmail.com>
2023-08-15 23:19:54 +0000

    dog.h file corrected

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
-
 /**
  * struct dog - Defines a structure for representing dog information.
  * @name: The name of the dog.
@@ -11,18 +9,12 @@
  *
  */
 
-struct dog
+typedef struct dog
 {
 	char *name; /* The name of the dog */
 	float age; /* The age of the dog */
 	char *owner; /* The owner of the dog */
-};
-
-/**
- * dog_t - typedef alias for struct dog
- */
-
-typedef struct dog dog_t;
+} dog_t; /*typedef alias for struct dog */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

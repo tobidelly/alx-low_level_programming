38b159135dd03eb6a100146509941dedc07c10fc
Tobintino <tobidelly@gmail.com>
2023-08-28 20:45:57 +0000

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node
+ * @head: pointer
+ *
+ * Return: data inside the element (Success), 0 if fails
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int num;
+
+	if (!head || !*head)
+		return (0);
+
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+C language Programming - More singly linked lists
+
+Check 0x12 for "Singly linked lists"

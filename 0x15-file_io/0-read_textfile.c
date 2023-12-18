1c41d59723298a4f78009773f4446a433b953264
tobidelly <tobintino@gmail.com>
2023-12-18 18:44:46 +0100

    file code update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * read_textfile- Read text file and print.
@@ -7,44 +8,22 @@
  * Return: number of bytes
  */
 
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;
-    ssize_t fd;
-    ssize_t w;
-    ssize_t t;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buf = malloc(sizeof(char) * (letters + 1));
-    if (buf == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-    t = read(fd, buf, letters);
-    if (t == -1)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
-
-    buf[t] = '\0';  // Null-terminate the string
-
-    w = write(STDOUT_FILENO, buf, t);
-    if (w == -1 || w != t)  // Check if write was successful and wrote all bytes
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
-
-    free(buf);
-    close(fd);
-    return (w);
+	free(buf);
+	close(fd);
+	return (w);
 }
